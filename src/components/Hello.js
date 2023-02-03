67815649475825e992c306f0a99c859d00995cc5
Juan Pablo Garzon Parra <juangarzon@192.168.0.11>
2023-02-03 18:17:47 -0500

    Hello page with some changes

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import "../styles/Hello.css";
+import Grid from "@mui/material/Unstable_Grid2";
+import { Container } from "@mui/system";
 
 function Hello() {
   return (
-    <div>Hello</div>
-  )
+    <>
+      <Container maxWidth="lg" className="container">
+        <Grid xs={8} className="grid">
+          <h1>Hey!</h1>
+          <h3>
+            I'm <span className="name">Juan Garzon</span>.
+          </h3>
+          <h4>Front End developer</h4>
+        </Grid>
+      </Container>
+    </>
+  );
 }
 
-export default Hello
+export default Hello;
