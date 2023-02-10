ee51b3be566f7adb0c2d424f59f9a36fb0815391
Juan Pablo Garzon Parra <juangarzon@Juans-MacBook-Air.local>
2023-02-10 10:37:26 -0500

    New photo in the about me section and added an introduce my self paragraph

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Grid from "@mui/material/Unstable_Grid2";
+import { Container } from "@mui/system";
+import "../styles/AboutMe.css";
+import PhotoAboutMe from "./PhotoAboutMe";
+import InformationAboutMe from "./InformationAboutMe";
 
 function AboutMe() {
   return (
-    <div>AboutMe</div>
-  )
+    <Container maxWidth="lg" className="aboutcontainer">
+      <Grid container spacing={3} maxWidth="100%" className='aboutgrid'>
+        <Grid md={12}>
+          <PhotoAboutMe />
+        </Grid>
+        <Grid md={6}>
+          <InformationAboutMe />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default AboutMe
+export default AboutMe;
---
+++ src/components/PhotoAboutMe.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'mui-image'
+
+
+function PhotoAboutMe()  {
+  return (
+    <>
+      <Image
+        src="https://media.licdn.com/dms/image/C4D16AQE983ik3jDxEQ/profile-displaybackgroundimage-shrink_350_1400/0/1627676066252?e=1681344000&v=beta&t=CVU8AB4SWbRqA2n5qiB4XW6AS7CvmJ3MY4xE50lpf-s"
+        alt="Juan Garzon"
+        aspectRatio={1}
+        color="#000"
+      ></Image>
+    </>
+  )
+}
+
+export default PhotoAboutMe
